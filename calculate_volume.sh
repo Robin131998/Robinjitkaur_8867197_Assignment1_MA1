3e81eee18feb908c20723e77bc9876854e0a1c97
robin <robinrandhawa20@gmail.com>
2025-02-05 14:58:27 -0500

    Add volume calculation logic and make script executable

---
+++ calculate_volume.sh
@@ -0,0 +1,12 @@
+
+
+calculate_volume() {
+  local height=$1
+  local width=$2
+  local length=$3
+  local volume=$((height * width * length))
+  echo "Volume: $volume"
+}
+
+
+calculate_volume 4 3 2
